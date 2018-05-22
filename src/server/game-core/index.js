bfa51ba3445d787d37c40dbf4e4742b0bb604203
olegon <olegon.main@gmail.com>
2018-05-22 08:45:11 -0300

    Serving frot-end game files

--- src/server/game-core/index.js
+++ src/server/game-core/index.js
@@ -1,15 +1,36 @@
 const path = require('path');
+const express = require('express');
 const socketIO = require('socket.io');
 
+// const roomService = require('./services/room-service');
 const playerService = require('./services/player-service')();
 const wordService = require('./services/word-service')();
 const gameService = require('./services/game-service')(playerService, wordService);
+// const pageService = require('./services/page-service');
 
 module.exports = function (app, server) {
     const io = socketIO(server);
 
+    app.use('/public', express.static(path.join(__dirname, '../../client/public')));
+
+    app.get('/logic', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/logic/index.html'));
+    });
+
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../../client/index.html'));
+    });
+
+    app.get('/rooms', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/rooms.html'));
+    });
+
+    app.get('/game', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/game.html'));
+    });
+
+    app.get('/new-game', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client/new-game.html'));
     });
 
     io.on('connection', (socket) => {

3e8804e3ea81d1f299f296879f7a86e19e9e7f11
olegon <olegon.main@gmail.com>
2018-06-19 19:51:35 -0300

    Removing ban button

--- src/client/public/js/game.js
+++ src/client/public/js/game.js
@@ -139,7 +139,7 @@
             $("#div-frozen").hide();
             $("#match").show();
         }
-        
+
         if (_player.confused) {
             $('body').addClass('confusion-applied');
         }
@@ -239,7 +239,7 @@
                 boxPlayers += `
                 <div class="col-md-` + boxSize + `" style="padding: 0;">
                     <div class="card stop-player-card ${_players[i].frozen ? 'player-is-frozen' : ''} ">
-                        <div class="card-header">` + _players[i].userName + `<span id="score-` + _players[i].playerId + `" class="player-score">Pontos: ` + _players[i].score + `&nbsp;&nbsp;<img class="stop-btn-ban-user" src="/public/img/ban-user.png" alt="ban user button" title="Banir usuário." /></span></div>
+                        <div class="card-header">` + _players[i].userName + `<span id="score-` + _players[i].playerId + `" class="player-score">Pontos: ` + _players[i].score + `&nbsp;&nbsp;</span></div>
                         <div class="card-body">
                             <div class="text-center" style="display: ${_player.canCastFrostPlayer ? 'inline' : 'none'};">
                                 <a href="#" data-power="frozen" data-target="` + _players[i].playerId + `">
